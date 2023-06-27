deeb13dd00b6c6b5305079d0cce88c018a317d89
chenjun <NicoIer@163.com>
2023-06-27 19:17:08 +0800

    feat: 泛型测试

--- Assets/HotUpdate/CubeComponent.cs
+++ Assets/HotUpdate/CubeComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HotUpdate
@@ -7,6 +8,12 @@ namespace HotUpdate
         void Start()
         {
             Debug.Log("CubeComponent Start");
+            List<CubeComponent> cubeComponents = new List<CubeComponent>();
+            cubeComponents.Add(this);
+            foreach (var component in cubeComponents)
+            {
+                Debug.Log(component);
+            }
         }
     }
 }
