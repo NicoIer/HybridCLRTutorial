d4cc5724eda7554e428ec6e87623cea767b2fe8a
chenjun <NicoIer@163.com>
2023-06-27 19:08:38 +0800

    feat: add move behavior

--- Assets/HotUpdate/KeyboardComponent.cs
+++ Assets/HotUpdate/KeyboardComponent.cs
@@ -12,6 +12,14 @@ namespace HotUpdate
 
         void Update()
         {
+            var hor = Input.GetAxis("Horizontal");
+            var ver = Input.GetAxis("Vertical");
+            if (Math.Abs(hor) > 0.01f || Math.Abs(ver) > 0.01f)
+            {
+                transform.position += new Vector3(hor, ver, 0);
+                Debug.Log($"Update hor:{hor} ver:{ver}");
+            }
+            
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Debug.Log("Update KeypadEnter Down");
