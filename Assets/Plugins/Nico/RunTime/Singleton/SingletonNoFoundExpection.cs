4abee07cc2a3a1aa368491bcf2cbabf669acdbe3
NicoIer <91652330+NicoIer@users.noreply.github.com>
2023-07-02 13:41:25 +0800

    import dialogSystem

---
+++ Assets/Plugins/Nico/RunTime/Singleton/SingletonNoFoundExpection.cs
@@ -0,0 +1,16 @@
+﻿namespace Nico
+{
+    public class DesignException: System.Exception
+    {
+        public DesignException()
+        {
+        }
+
+        public DesignException(string msg) : base(msg)
+        {
+            
+        }
+        
+        
+    }
+}
